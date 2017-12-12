a81d109f9b60367db50bb773a25f2331bd2f2518
Peter Williams <retex.williams@gmail.com>
2017-12-12 23:25:21 +0000

    added query strings

--- app.js
+++ app.js
@@ -1,56 +1,25 @@
-var http = require('http');
-// File server for reading / writing files
-var fs = require('fs');
+var express = require('express');
 
+var app = express();
 
+app.set('view engine', 'ejs');
 
+// Middleware
+app.use('/assets', express.static('assets'));
 
+app.get('/', function(req, res){
+  res.render('index');
+});
 
+app.get('/contact', function(req, res){
+  res.render('contact', {qs: req.query});
+});
 
-
-var server = http.createServer(function(req, res){
-  console.log('request was made: ' + req.url);
-  // res.writeHead(200, {'Content-Type': 'text/html'});
-  // var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-  // myReadStream.pipe(res);
-  if(req.url === '/home' || req.url === '/'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  }
-  else if(req.url === '/contact'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-  }
-  else if (req.url === '/api/ninjas'){
-    var ninjas = [
-      {
-        name: 'ryu',
-        age: 29
-      },
-      {
-        name: 'yoshi',
-        age: 32
-      }
-    ];
-
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify(ninjas));
-  }
-  else {
-    res.writeHead(400, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
-  // res.writeHead(200, {'Content-Type': 'text/plain'});
-  // var myObj = {
-  //   name: 'Ryu',
-  //   job: 'Ninja',
-  //   age: 29
-  // }
-
-  // res.end(JSON.stringify(myObj));
-  // res.end('Feed me popcorn');
+app.get('/profile/:name', function(req, res){
+  var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+  res.render('profile', {person: req.params.name, data: data});
 });
 
 
-server.listen(3000, '127.0.0.1');
-console.log('Server listening on port 3000');
+app.listen(3000);
+
